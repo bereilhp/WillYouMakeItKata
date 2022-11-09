8939a468d4b78876fcddeaf918fef2da031d1272
PedroB <bereilhp@gmail.com>
2022-11-09 10:44:41 +0100

    New test have much more fuel

--- src/fuelCalc.test.js
+++ src/fuelCalc.test.js
@@ -7,3 +7,7 @@ test("Check if you can make it with fuel", () => {
 test("Check if you can't make it with fuel", () => {
     expect(fuel(50,25,1)).toBe(false);
 })
+
+test("Check if you have much more fuel and can make it", () => {
+    expect(fuel(50,25,4)).toBe(true);
+})
